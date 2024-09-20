cf0388353bc7b9a3123d0b97f8c3100efde28abe
Joey <joeyrjtd@gmail.com>
2024-09-20 15:09:55 -0300

    inject film repository

--- api/lib/src/film_repository/mod.rs
+++ api/lib/src/film_repository/mod.rs
@@ -1,6 +1,10 @@
 use shared::models::{CreateFilm, Film};
 use uuid::Uuid;
 
+mod postgres_film_repository;
+
+pub use postgres_film_repository::PostgresFilmRepository;
+
 pub type FilmError = String;
 pub type FilmResult<T> = Result<T, FilmError>;
 
